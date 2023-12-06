56866290f26b356ec138eb6f9abb4dca7d457e35
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-12-06 14:59:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,13 @@ show_chart = st.sidebar.checkbox('Show Runs Chart')
 if show_chart:
     st.bar_chart(df2.set_index('Batsmen')) 
 
+Wicket ={'Bowlers':['M Shami','A Zampa','S Madushanka','J Bumrah','G Coetzee','S Afridi','M Jensen','R Jadeja','J Hazlewood','M Santner'],
+         'Wickets':[24,23,21,20,20,18,17,16,16,16]}
+df3 = pd.DataFrame(Wicket)
+show_chart = st.sidebar.checkbox('Show Line Chart')
+if show_chart:
+    st.line_chart(df3.set_index('Bowlers'))
+
 Table ={'TEAMS':['IND','AUS','RSA','NZ','PAK','AFG','ENG','BAN','SN','NED'],
         'POINTS':[18,14,14,10,8,8,6,4,4,4]}
 df1= pd.DataFrame(Table)
@@ -65,12 +72,12 @@ if st.sidebar.button('Per'):
 # if show_chart:
 #     st.bar_chart(df2.set_index('Batsmen'))            
                  
-Wicket ={'Bowlers':['M Shami','A Zampa','S Madushanka','J Bumrah','G Coetzee','S Afridi','M Jensen','R Jadeja','J Hazlewood','M Santner'],
-         'Wickets':[24,23,21,20,20,18,17,16,16,16]}
-df3 = pd.DataFrame(Wicket)
-show_chart = st.sidebar.checkbox('Show Line Chart')
-if show_chart:
-    st.line_chart(df3.set_index('Bowlers'))
+# Wicket ={'Bowlers':['M Shami','A Zampa','S Madushanka','J Bumrah','G Coetzee','S Afridi','M Jensen','R Jadeja','J Hazlewood','M Santner'],
+#          'Wickets':[24,23,21,20,20,18,17,16,16,16]}
+# df3 = pd.DataFrame(Wicket)
+# show_chart = st.sidebar.checkbox('Show Line Chart')
+# if show_chart:
+#     st.line_chart(df3.set_index('Bowlers'))
 
 
 
