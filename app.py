2a9e5138e48a5ef9bf649c6187e420339d29297a
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2023-12-06 11:47:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,13 @@ show_chart = st.sidebar.checkbox('Show Line Chart')
 if show_chart:
     st.line_chart(df3.set_index('Bowlers'))
 
+Wicket ={'Bowlers':['M Shami','A Zampa','S Madushanka','J Bumrah','G Coetzee','S Afridi','M Jensen','R Jadeja','J Hazlewood','M Santner'],
+         'Wickets':[24,23,21,20,20,18,17,16,16,16]}
+df3 = pd.DataFrame(Wicket)
+show_chart = st.sidebar.checkbox('Show Line Chart')
+if show_chart:
+    st.line_chart(df3.set_index('Bowlers'))
+
 # Score={'Batsmen':['V Kohli','R Sharma','De Kock','R Ravindra','D Mitchell','D Warner','S Iyer','KL Rahul','Van der Dussen','M Marsh'],
 #        'Runs':[765,597,594,578,552,535,452,448,441]}
 # df2 = pd.DataFrame(Score)
